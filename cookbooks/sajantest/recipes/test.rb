b164ecdb5dce3940cd4f167232a14cc8123a50c9
Sajan Tharayil <sajangnn@gmail.com>
2012-07-16 11:08:18 +0000

    Changing the test recipie space issue

--- cookbooks/sajantest/recipes/test.rb
+++ cookbooks/sajantest/recipes/test.rb
@@ -1,17 +1,15 @@
 hostname_fqdn = "#{node.sajantest.rs_hostname}".downcase
-
 template "/etc/hostname" do
-    source "hostname.erb"
-    owner "root"
-    group "root"
-    mode "0644"
-    variables(
-      :hostname_fqdn => hostname_fqdn
-    )
-  end
-
+  source "hostname.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+    :hostname_fqdn => hostname_fqdn
+  )
+end
 bash "set_hostname" do
-    code << EOH
-      start hostname
-    EOH
-  end
+  code <<-EOH
+    start hostname
+  EOH
+end
